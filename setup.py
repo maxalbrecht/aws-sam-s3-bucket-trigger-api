f23e3a072da203b0393d62d86559038306807d27
maxalbrecht100 <max.albrecht100@gmail.com>
2019-12-11 15:15:37 -0500

    debug

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ import sys
 from distutils.core import setup
 
 
-
 setup(
     name='S3 Bucket to API',
     version='0.1dev',
     package_dir = {'': '.venv/Lib/site-packages'},
-    packages=['boto3', 'requests'],
+    packages=['boto3', 'botocore', 'requests'],
     long_description=open('README.md').read(),
 )
