ad508520a5d763ea653d3a9fb7ddf7d8557e6f25
maxalbrecht100 <max.albrecht100@gmail.com>
2019-12-11 14:30:21 -0500

    changes to host documentation

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from distutils.core import setup
+
+setup(
+    name='S3 Bucket to AOT',
+    version='0.1dev',
+    packages=['distutils', 'json', 'boto3', 'requests', 'os', 'uuid'],
+    long_description=open('README.txt').read(),
+)
