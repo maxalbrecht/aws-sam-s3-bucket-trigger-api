9b7a7884c7f35419ea37aee6a5081d3f91baa6fc
maxalbrecht100 <max.albrecht100@gmail.com>
2019-12-11 16:40:55 -0500

    debugging generating html for main function

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ from distutils.core import setup
 setup(
     name='S3 Bucket to API',
     version='0.1dev',
+    author='Max Albrecht',
+    author_email='max.albrecht100@gmail.com',
     package_dir = {'': '.venv/Lib/site-packages'},
-    packages=['boto3', 'botocore', 'botocore.vendored'],
+    packages=['requests', 'boto3', 'botocore', 'botocore.vendored'],
     long_description=open('README.md').read(),
 )
