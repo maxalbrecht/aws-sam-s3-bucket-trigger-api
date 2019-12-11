d12f7870f315d139774af66117ac57fc3b3a8a8a
maxalbrecht100 <max.albrecht100@gmail.com>
2019-12-11 18:03:56 -0500

    test

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author='Max Albrecht',
     author_email='max.albrecht100@gmail.com',
     package_dir = {'': '.venv/Lib/site-packages'},
-    packages=['requests', 'boto3', 'botocore',],
+    packages=['requests', 'boto3', 'botocore', 'botocore.vendored',],
     long_description=open('README.md').read(),
 )
