96cad5f3e4dc02621d9d02f97cc219ccd2a4b657
maxalbrecht100 <max.albrecht100@gmail.com>
2019-12-11 14:57:29 -0500

    debug

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ import os
 import sys
 from distutils.core import setup
 
-package_dir = {'': '.venv/Lib/site-packages'}
+
 
 setup(
     name='S3 Bucket to AOT',
     version='0.1dev',
+    package_dir = {'': '.venv/Lib/site-packages'},
     packages=['boto3', 'requests', 'uuid'],
     long_description=open('README.md').read(),
 )
