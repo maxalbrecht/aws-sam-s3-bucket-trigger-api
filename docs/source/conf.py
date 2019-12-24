10b11fb5d5082b9d7774dee599c8cab9ee79450f
maxalbrecht100 <max.albrecht100@gmail.com>
2019-12-24 13:14:48 -0500

    edit docs options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,3 +106,9 @@ outputfile.close()
 
 # Disable source link
 html_show_sourcelink = False
+html_context = {
+"display_github": False, # Add 'Edit on Github' link instead of 'View page source'
+"last_updated": True,
+"commit": False,
+}
+
