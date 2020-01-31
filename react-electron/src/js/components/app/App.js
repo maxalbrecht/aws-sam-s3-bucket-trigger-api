9b1aec2469133bd5ea6d3059935dd8ddfe3f94e7
maxalbrecht100 <max.albrecht100@gmail.com>
2020-01-31 10:29:19 -0500

    cleanup and project structure adjustments

--- react-electron/src/js/components/app/App.js
+++ react-electron/src/js/components/app/App.js
@@ -1,52 +1,16 @@
+import { mapDispatchToProps, logicConstructor } from './App.logic/App.logic'
 import React, { Component } from 'react';
-import { Form, Col } from 'react-bootstrap';
-import SubmittedJobsList from "./../list/List";
-import './App.css';
+import { Form, Col} from 'react-bootstrap';
 import { connect } from "react-redux";
-
-// Importing logic functions
-import {
-  mapDispatchToProps,
-  handleClickBrowse,
-  updatePriorityOptions,
-  handleChange,
-  handleSubmit,
-  onDragStart,
-  onDragUpdate,
-  onDragEnd,
-  getConstructorState
-} from './AppFunctions'
-
-// Importing UI Elements
-import JobNumber from './fields/jobNumber'
-import SourceFiles from './fields/sourceFiles'
-import OrderType from './fields/orderType'
-import Priority from './fields/priority'
-import Notes from './fields/notes'
-import SubmitJobButton from './fields/submitJobButton'
+import './App.css';
+import SubmittedJobsList from "../list/List";
+import fieldBind from './fields';
 
 class ConnectedApp extends Component {
   constructor(props) {
     super(props);
-    this.state = getConstructorState();
-
-    // Binding imported logic functions
-    this.mapDispatchToProps = mapDispatchToProps.bind(this);
-    this.handleClickBrowse = handleClickBrowse.bind(this);
-    this.updatePriorityOptions = updatePriorityOptions.bind(this);
-    this.handleChange = handleChange.bind(this);
-    this.handleSubmit = handleSubmit.bind(this);
-    this.onDragStart = onDragStart.bind(this);
-    this.onDragUpdate = onDragUpdate.bind(this);
-    this.onDragEnd = onDragEnd.bind(this);
-
-    // Binding imported UI Elements
-    this.JobNumber = JobNumber.bind(this);
-    this.SourceFiles = SourceFiles.bind(this);
-    this.OrderType = OrderType.bind(this);
-    this.Priority = Priority.bind(this);
-    this.Notes = Notes.bind(this);
-    this.SubmitJobButton = SubmitJobButton.bind(this);
+    logicConstructor.bind(this)(props);
+    fieldBind.bind(this)();
   }
 
   render() {
