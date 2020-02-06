f2efa426c6cdee84ce88de5850523974f24db21c
maxalbrecht100 <max.albrecht100@gmail.com>
2020-02-06 16:14:19 -0500

    Update background color of failed jobs

--- react-electron/src/js/constants/list_item_colors.js
+++ react-electron/src/js/constants/list_item_colors.js
@@ -4,5 +4,5 @@ export const COLOR_DEFAULT = 'darkgrey';
 //export const COLOR_SUCCESS = '#3F7EB3';
 //export const COLOR_SUCCESS = '#00224B';
 export const COLOR_SUCCESS = rgbToHex(0, 34, 75);
-export const COLOR_ERROR = '#B35F98';
-//export const COLOR_ERROR = '#3E0000';
+//export const COLOR_ERROR = '#B35F98';
+export const COLOR_ERROR = '#3E0000';
