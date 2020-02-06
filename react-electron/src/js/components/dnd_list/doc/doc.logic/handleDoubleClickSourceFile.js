6151269a4f9db5df677d7d25f2bbed165b5acbe8
maxalbrecht100 <max.albrecht100@gmail.com>
2020-02-06 16:11:52 -0500

    Allow User to Double Click Source Files to Open Them in Text Editor

---
+++ react-electron/src/js/components/dnd_list/doc/doc.logic/handleDoubleClickSourceFile.js
@@ -0,0 +1,12 @@
+const { shell } = window.require('electron')
+
+function handleDoubleClickSourceFile(e) {
+  if (this.props.doc.content.toLowerCase().endsWith(".txt")) {
+    console.log("opening the following file:");
+    console.log(this.props.doc.content);
+    
+    shell.openExternal(this.props.doc.content);
+  }
+}
+
+export default handleDoubleClickSourceFile;
