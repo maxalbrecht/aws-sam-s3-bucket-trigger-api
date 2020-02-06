3222db4a0efcc1b4ec31e6632420cc286219eb9c
maxalbrecht100 <max.albrecht100@gmail.com>
2020-02-06 16:11:20 -0500

    Add logo

--- react-electron/src/js/components/footer_bar/FooterBar.js
+++ react-electron/src/js/components/footer_bar/FooterBar.js
@@ -4,26 +4,29 @@ import './FooterBar.css'
 
 class FooterBar extends Component {
   render(){
+    /*
+    <Link className="App-link" to="/">
+      Main
+    </Link> | <Link className="App-link" to="/about">
+      About
+    </Link> | <Link className="App-link"  to={{
+        pathname: 'documentation',
+        state: {
+          docsLocation: process.env.PUBLIC_URL + '/docs/build/html/index.html'
+        }
+      }}
+    >
+      Documentation
+    </Link>
+    */
+  
     return (
-      <div id="footer-bar">
-        <Link className="App-link" to="/">
-          Main
-        </Link> | <Link className="App-link" to="/about">
-          About
-        </Link> | <Link className="App-link"  to={{
-            pathname: 'documentation',
-            state: {
-              docsLocation: process.env.PUBLIC_URL + '/docs/build/html/index.html'
-            }
-          }}
-        >
-          Documentation
-        </Link>
+      <div id="footer-bar" style={{minHeight:'60px'}}>
         <img 
           style={{
             position:'absolute',
             right:'15px',
-            top:'-10px'
+            top:'-0px'
           }}
           id="bottom-right-logo"
           src={process.env.PUBLIC_URL + '/SmallLogoNew.png'} 
