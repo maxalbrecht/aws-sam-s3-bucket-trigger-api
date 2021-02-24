f29e9e23e92b0da3a2e296250a5e6b2c132d65c0
maxalbrecht100 <max.albrecht100@gmail.com>
2021-02-23 23:27:48 -0500

    add log statements to mfa-confirm function

--- react-electron/src/js/components/auth/login/login.logic/handleConfirm.js
+++ react-electron/src/js/components/auth/login/login.logic/handleConfirm.js
@@ -3,6 +3,7 @@ import { MFA_SETUP, SOFTWARE_TOKEN_MFA } from './../../../../constants/cognito_c
 import User from './../../../../classes/user/user'
 import { action } from './../../../../utils/action'
 import { USER_LOGGED_IN } from './../../../../constants/action-types'
+import Logging from './../../../../utils/logging'
 import defined from './../../../../utils/defined'
 var store = window.store;
 
@@ -17,27 +18,36 @@ async function handleConfirm(event) {
   let errorPresent = this.ValidateLoginFields();
 
   try{
+    Logging.LogSectionStart()
+    Logging.Log("auth.login.login.logic.handleConfirm() about to try to either check or set up MFA")
+    Logging.LogEach("this.state:", this.state)
+
     if (errorPresent === false && defined(this.state.cognitoUser)) {
       const { cognitoUser, confirm } = this.state;
 
       //// ALLOW USERS WHO ARE SETTING UP MFA FOR THE FIRST TIME TO DO SO AND LOG IN 
       if(defined(cognitoUser) && cognitoUser.challengeName === MFA_SETUP) {
+        Logging.LogEach("ALLOW USERS WHO ARE SETTING UP MFA FOR THE FIRST TIME TO DO SO AND LOG IN", "CONFIRM TOKEN")
         //CONFIRM TOKEN
         await Auth.verifyTotpToken(cognitoUser, confirm)
 
         // SET PREFERRED MFA
+        Logging.Log("SET PREFERRED MFA")
         await Auth.setPreferredMFA(await Auth.currentAuthenticatedUser(), 'TOTP')
       }
       //// ALLOW USERS THAT ARE ALREADY SET UP WITH MFA TO LOG IN
       else if(defined(cognitoUser) && cognitoUser.challengeName === SOFTWARE_TOKEN_MFA) {
+        Logging.Log("ALLOW USERS THAT ARE ALREADY SET UP WITH MFA TO LOG IN")
         await Auth.confirmSignIn(cognitoUser, confirm, SOFTWARE_TOKEN_MFA);
       }
 
       // SET STATE FOR THE LOGIN COMPONENT
+      Logging.Log("SET STATE FOR THE LOGIN COMPONENT")
       let newState = { ...this.state };
       newState.attemptingConfirm = true
       newState.cognitoUser = await Auth.currentAuthenticatedUser()
       this.setState(newState);
+      Logging.LogEach("newState:", newState)
 
       // SET STATE FOR THE REDUX STORE
       let payload = {
@@ -48,6 +58,7 @@ async function handleConfirm(event) {
       
       this.props.history.push("/app");
     }
+    Logging.LogSectionEnd()
   } catch (error) {
     let e = null;
     !error.message ? e = { "message" : error } : e = error;
